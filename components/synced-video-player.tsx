a783f86a8434f09dbbfeefe9bc6c28c263360cfc
siamshariar <siam.it.int@gmail.com>
2026-03-09 17:04:28 +0600

    Update synced-video-player.tsx

--- components/synced-video-player.tsx
+++ components/synced-video-player.tsx
@@ -261,7 +261,7 @@ const StartScreen = ({ onPlayClick }: { onPlayClick: () => void }) => {
             <img 
               src="/DeeniTV-V-2.png" 
               alt="Deeni.tv - Your Spiritual TV Experience"
-              className={isMobile ? 'h-8' : isTablet ? 'h-10' : 'h-12'}
+              className={isMobile ? 'h-5' : isTablet ? 'h-10' : 'h-12'}
             />
           </motion.div>
           
@@ -394,7 +394,7 @@ const TapToUnmuteScreen = ({ onUnmuteClick }: { onUnmuteClick: () => void }) => 
             <img
               src="/DeeniTV-V-2.png"
               alt="Deeni.tv"
-              className={isMobile ? 'h-5' : isTablet ? 'h-8' : 'h-12'}
+              className={isMobile ? 'h-8' : isTablet ? 'h-10' : 'h-12'}
             />
           </motion.div>
 
